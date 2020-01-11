51f7b12848ce83ff1baf2a70bd3a614af276933a
mawentao <mwt_luoyang@126.com>
2020-01-11 18:34:16 +0800

    2020 1 11

--- next.config.js
+++ next.config.js
@@ -8,34 +8,3 @@ module.exports = {
     return config
   }
 }
-// const withCss = require('@zeit/next-css')
-// // if (typeof require !== 'undefined') {
-// //   require.extensions['.css'] = file => { }
-// // }
-
-// // module.exports = withCss({})
-// module.exports = withCss({
-//   // webpack: (config, { isServer }) => {
-//   //   if (isServer) {
-//   //     const antStyles = /antd\/.*?\/style\/css.*?/
-//   //     const origExternals = [...config.externals]
-//   //     config.externals = [
-//   //       (context, request, callback) => {
-//   //         if (request.match(antStyles)) return callback()
-//   //         if (typeof origExternals[0] === 'function') {
-//   //           origExternals[0](context, request, callback)
-//   //         } else {
-//   //           callback()
-//   //         }
-//   //       },
-//   //       ...(typeof origExternals[0] === 'function' ? [] : origExternals),
-//   //     ]
-
-//   //     config.module.rules.unshift({
-//   //       test: antStyles,
-//   //       use: 'null-loader',
-//   //     })
-//   //   }
-//   //   return config
-//   // },
-// })
